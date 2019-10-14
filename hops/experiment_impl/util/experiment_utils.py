4af639e3b412cd41e1c0738a7752bbfa4b58af0a
Moritz Meister <meister.mo@gmail.com>
2019-10-14 15:07:32 +0200

    Remove prints

--- hops/experiment_impl/util/experiment_utils.py
+++ hops/experiment_impl/util/experiment_utils.py
@@ -400,8 +400,8 @@ def _attach_experiment_xattr(app_id, run_id, json_data, xattr):
                    app_id + "_" + str(run_id) + "?xattr=" + xattr
 
     resp = util.send_request('POST', resource_url, data=json_data, headers=headers)
-    print(resource_url)
-    print(resp)
+    #print(resource_url)
+    #print(resp)
 
 def _attach_model_link_xattr(ml_id, model, xattr):
     """
