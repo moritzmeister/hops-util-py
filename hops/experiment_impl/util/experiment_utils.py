a1edc77ab18c900b5085cda2ba5edf15011b2092
moritzmeister <meister.mo@gmail.com>
2020-06-04 13:05:02 +0200

    fix return dict

--- hops/experiment_impl/util/experiment_utils.py
+++ hops/experiment_impl/util/experiment_utils.py
@@ -734,7 +734,7 @@ def _get_metric(return_dict, metric_key):
     if return_dict and metric_key:
         assert metric_key in return_dict.keys(), 'Supplied metric_key {} is not in returned dict {}'.format(metric_key, return_dict)
         return str(return_dict[metric_key])
-    elif return_dict and len(return_dict.keys()) == 1 and 'metric' in return_dict.keys():
+    elif return_dict and len(return_dict.keys()) == 2 and 'metric' in return_dict.keys():
         return str(return_dict['metric'])
     else:
         return None
